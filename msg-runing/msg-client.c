b93f1b10f688a1a3f791806dc16de83556c3dab0
huayadong <huayadong@kylinos.cn>
2020-07-01 15:48:23 +0800

    add bwrap

--- msg-runing/msg-client.c
+++ msg-runing/msg-client.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/msg.h>
-#include<dirent.h>
-#include<sys/ipc.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/msg.h>
+#include <dirent.h>
+#include <sys/ipc.h>
+#include <unistd.h>
 
-int find_pid_by_name( char* ProcName)
+int find_pid_by_name(char *ProcName)
 {
-        DIR             *dir;
-        struct dirent   *d;
-        int             pid;
-        char            *s;
+        DIR *dir;
+        struct dirent *d;
+        int pid;
+        char *s;
         int pnlen;
-        pnlen = strlen(ProcName);//名字长度
+        pnlen = strlen(ProcName); //名字长度
         /* 打开文件夹 /proc */
         dir = opendir("/proc");
         if (!dir)
@@ -21,105 +21,140 @@ int find_pid_by_name( char* ProcName)
                 return 0;
         }
         /* 遍历文件夹 */
-        while ((d = readdir(dir)) != NULL) {
-                char exe [PATH_MAX+1];
-                char path[PATH_MAX+1];
+        while ((d = readdir(dir)) != NULL)
+        {
+                char exe[PATH_MAX + 1];
+                char path[PATH_MAX + 1];
                 int len;
                 int namelen;
                 /* 看看是不是一个进程,(名字是不是数字字符) */
-                if ((pid = atoi(d->d_name)) == 0)       continue;
+                if ((pid = atoi(d->d_name)) == 0)
+                        continue;
                 snprintf(exe, sizeof(exe), "/proc/%s/exe", d->d_name);
                 /*取得符号连接所指的文件，存入path*/
                 if ((len = readlink(exe, path, PATH_MAX)) < 0)
                         continue;
                 path[len] = '\0';
                 s = strrchr(path, '/');
-                if(s == NULL) continue;
+                if (s == NULL)
+                        continue;
                 s++;
                 /* 名字长度不够的不要 */
                 namelen = strlen(s);
-                if(namelen < pnlen)     continue;
+                if (namelen < pnlen)
+                        continue;
                 /*判断名字长度是否相等*/
-                if(!strncmp(ProcName, s, pnlen)) {
-                /* 判断名字的最后是不是空格或'\0' */
-                     if(s[pnlen] == ' ' || s[pnlen] == '\0') {
-                         return 1;
-                     }
+                if (!strncmp(ProcName, s, pnlen))
+                {
+                        /* 判断名字的最后是不是空格或'\0' */
+                        if (s[pnlen] == ' ' || s[pnlen] == '\0')
+                        {
+                                return 1;
+                        }
                 }
         }
         closedir(dir);
-        return  0;
+        return 0;
 }
 
-struct mymesg{
-	long int mtype;
-	char mtext[512];
+struct mymesg
+{
+        long int mtype;
+        char mtext[512];
 };
 
-int msgsend()
+int msgsend(char *argv)
 {
         int id = 0;
         struct mymesg ckxmsg;
-        key_t key = ftok("/tmp",66);
-        id = msgget(key,IPC_CREAT | 0666);
-        if(id == -1)
+        key_t key = ftok("/tmp", 66);
+        id = msgget(key, IPC_CREAT | 0666);
+        if (id == -1)
         {
                 printf("create msg error \n");
                 return 0;
         }
 
-                char msg[512];
-                memset(msg,0,sizeof(msg));
-                ckxmsg.mtype = 1;
-                printf("input message:");
-                fgets(msg,sizeof(msg),stdin);
-                strcpy(ckxmsg.mtext,msg);
+        char msg[512];
+        memset(msg, 0, sizeof(msg));
+        ckxmsg.mtype = 1;
+        strcpy(ckxmsg.mtext, argv);
 
-                if(msgsnd(id,(void *)&ckxmsg,512,0) < 0)
-                {
-                        printf("send msg error \n");
-                        return 0;
-                }
-
-/*	if(msgctl(id,IPC_RMID,NULL) < 0)//删除返回信息msg
+        if (msgsnd(id, (void *)&ckxmsg, 512, 0) < 0)
         {
-                printf("del msg error \n");
+                printf("send msg error \n");
                 return 0;
         }
-        return 0;
-	*/
 }
 
-int msgrcvget()
+int msgrcvget(char *argv)
 {
         int id = 0;
         struct mymesg ckxmsg;
-        key_t key = ftok("/tmp/1",66);
-        id = msgget(key,IPC_CREAT | 0666);
-        if(id == -1)
+        key_t key = ftok("/tmp/1", 66);
+        id = msgget(key, IPC_CREAT | 0666);
+        if (id == -1)
         {
                 printf("create msg error \n");
                 return 0;
         }
 
-
-                if(msgrcv(id,(void *)&ckxmsg,512,1,0) < 0)
-                {
-                        printf("receive msg error \n");
-                        return 0;
-                }
-                printf("data:%s\n",ckxmsg.mtext);
+        if (msgrcv(id, (void *)&ckxmsg, 512, 1, 0) < 0)
+        {
+                printf("receive msg error \n");
+                return 0;
+        }
+        printf("%s:%s\n", argv, ckxmsg.mtext);
 
         return 0;
 }
 
-int main()
+void bwrap_run(char *argv)
 {
-	if(!find_pid_by_name("msg-service")){
-		puts("The server 'msg-service' is not running");
-	//	return 0;
-	}
-	msgsend();
-	msgrcvget();
-	return 0;
+        char str[10240];
+        sprintf(str, "bwrap \
+                --bind  /opt/%s/bin  /bin \
+                --bind  /opt/%s/boot /boot \
+                --ro-bind /dev /dev   \
+                --bind  /opt/%s/etc /etc \
+                --bind  /opt/%s/home  /home \
+                --bind  /opt/%s/lib  /lib \
+                --bind  /opt/%s/lib64 /lib64\
+                --bind  /opt/%s/media /media\
+                --bind  /opt/%s/mnt /mnt\
+                --bind  /opt/%s/opt  /opt\
+                --ro-bind  /proc /proc\
+                --bind  /opt/%s/root /root\
+                --bind  /run /run\
+                --bind  /opt/%s/sbin /sbin\
+                --bind  /opt/%s/srv /srv\
+                --ro-bind /sys /sys\
+                --bind  /tmp /tmp\
+                --bind  /opt/%s/usr /usr\
+                --bind  /opt/%s/var /var\
+                --bind /home /home\
+                --bind /etc/passwd  /etc/passwd\
+                --bind /etc/group   /etc/group\
+                %s ", argv, argv, argv, argv, argv, argv, argv, argv, argv, argv, argv, argv, argv, argv, argv);
+	puts(str);
+	system(str);
+
+}
+
+int main(int argc, char *argv[])
+{
+        if (argc < 2)
+        {
+                puts("please input appname");
+                return 0;
+        }
+        if (!find_pid_by_name("msg-service"))
+        {
+                //puts("The server 'msg-service' is not running");
+                //return 0;
+        }
+        msgsend(argv[1]);
+        msgrcvget(argv[1]);
+	bwrap_run(argv[1]);
+        return 0;
 }
--- msg-runing/msg-service.c
+++ msg-runing/msg-service.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
-#include<syslog.h> 
-#include<stdarg.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/msg.h>
-#include<sys/ipc.h>
+#include <stdio.h>
+#include <syslog.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
 
-struct mymesg{
+struct mymesg
+{
         long int mtype;
         char mtext[512];
 };
@@ -15,71 +16,70 @@ int msgsend()
 {
         int id = 0;
         struct mymesg ckxmsg;
-        key_t key = ftok("/tmp/1",66);
-        id = msgget(key,IPC_CREAT | 0666);
-        if(id == -1)
+        key_t key = ftok("/tmp/1", 66);
+        id = msgget(key, IPC_CREAT | 0666);
+        if (id == -1)
         {
                 printf("create msg error \n");
                 return 0;
         }
-                char msg[512];
-                memset(msg,0,sizeof(msg));
-                ckxmsg.mtype = 1;
+        char msg[512];
+        memset(msg, 0, sizeof(msg));
+        ckxmsg.mtype = 1;
 
-		strcpy(ckxmsg.mtext,"挂载成功");
-                if(msgsnd(id,(void *)&ckxmsg,512,0) < 0)
-                {
-                        printf("send msg error \n");
-                        return 0;
-                }
+        strcpy(ckxmsg.mtext, "挂载成功");
+        if (msgsnd(id, (void *)&ckxmsg, 512, 0) < 0)
+        {
+                printf("send msg error \n");
+                return 0;
+        }
         return 0;
 }
 
 int main()
 {
-	char mount[1024];
+        char mount[1024];
         char umount[1024];
-	int id = 0;
-	struct mymesg ckxmsg;
-	key_t key = ftok("/tmp",66);
-	id = msgget(key,0666|IPC_CREAT);
-	if(id == -1)
-	{
-		printf("open msg error \n");
-		return 0;
-	}
+        int id = 0;
+        struct mymesg ckxmsg;
+        key_t key = ftok("/tmp", 66);
+        id = msgget(key, 0666 | IPC_CREAT);
+        if (id == -1)
+        {
+                printf("open msg error \n");
+                return 0;
+        }
 
-	while(1)
-	{
-		if(msgrcv(id,(void *)&ckxmsg,512,1,0) < 0)
-		{
-			printf("receive msg error \n");
-			return 0;
-		}
-		printf("data:%s\n",ckxmsg.mtext);
+        while (1)
+        {
+                if (msgrcv(id, (void *)&ckxmsg, 512, 1, 0) < 0)
+                {
+                        printf("receive msg error \n");
+                        return 0;
+                }
+                printf("data:%s\n", ckxmsg.mtext);
 
-		ckxmsg.mtext[strlen(ckxmsg.mtext)-1]=0;
-                
-		memset(umount, 0, sizeof(umount));
+                //ckxmsg.mtext[strlen(ckxmsg.mtext) - 1] = 0;//删除末尾字符
+
+                memset(umount, 0, sizeof(umount));
                 memset(mount, 0, sizeof(mount));
-                
-		sprintf(umount, "umount /opt/%s",ckxmsg.mtext);
+
+                sprintf(umount, "umount /opt/%s", ckxmsg.mtext);
                 system(umount);
-               // puts(umount);
-		syslog(LOG_DEBUG, "msg-service------------------------------: '%s'\n",  umount); 
+                // puts(umount);
+                syslog(LOG_DEBUG, "msg-service------------------------------: '%s'\n", umount);
 
                 sprintf(mount, "mount -t aufs -o  dirs=/opt/%s=rw:/opt/app_runtime=ro:/opt/base_runtime=ro none /opt/%s", ckxmsg.mtext, ckxmsg.mtext);
                 system(mount);
-               // puts(mount);
-		syslog(LOG_DEBUG, "msg-service------------------------------: '%s'\n",  mount);
+                // puts(mount);
+                syslog(LOG_DEBUG, "msg-service------------------------------: '%s'\n", mount);
 
-		msgsend();
-
-	}
-        if(msgctl(id,IPC_RMID,NULL) < 0)//删除接收信息msg
+                msgsend(); //挂载完成后通知客户端。
+        }
+        if (msgctl(id, IPC_RMID, NULL) < 0) //删除接收信息msg
         {
                 printf("del msg error \n");
                 return 0;
         }
-	return 0;
+        return 0;
 }
