d17229d4d2b4a5ba2aef856290289ae15447cdb2
huayadong <huayadong@kylinos.cn>
2020-07-03 15:56:58 +0800

    add four orders

--- msg-runing/msg-client.c
+++ msg-runing/msg-client.c
@@ -113,6 +113,31 @@ int msgrcvget(char *argv)
         return 0;
 }
 
+void split(char *src, const char *separator, char **dest)
+{
+        /*
+        src 源字符串的首地址(buf的地址) 
+        separator 指定的分割字符
+        dest 接收子字符串的数组
+        */
+        char *pNext;
+        int count = 0;
+        
+        if (src == NULL || strlen(src) == 0) //如果传入的地址为空或长度为0，直接终止
+                return;
+
+        if (separator == NULL || strlen(separator) == 0) //如未指定分割的字符串，直接终止
+                return;
+
+        pNext = (char *)strtok(src, separator); //必须使用(char *)进行强制类型转换(虽然不写有的编译器中不会出现指针错误)
+        //puts(pNext);
+        while (pNext != NULL)
+        {
+                *dest++ = pNext;
+                pNext = (char *)strtok(NULL, separator); //必须使用(char *)进行强制类型转换
+        }
+}
+
 void bwrap_run(char *argv)
 {
         char str[10240];
@@ -153,7 +178,8 @@ int main(int argc, char *argv[])
                 //puts("The server 'msg-service' is not running");
                 //return 0;
         }
-        
+
+        char *appmsg_all[1] = {0};
         char msg_all[10240];
         memset(msg_all, 0, sizeof(msg_all));
         memset(app_name, 0, sizeof(app_name));
@@ -164,36 +190,69 @@ int main(int argc, char *argv[])
         {
                 puts("-h    or  --help                              打印帮助(本信息)并退出.");
                 puts("-d    app_name                                默认地址挂载运行");
-                puts("-p    base_path   local_path   app_name       手动指定地址");
+                puts("-b    app_name@platform_alias                 指定base_runtime平台 挂载运行");
+                puts("-c    appName     localdir                    指定local_runtimer平台挂载运行");
+                puts("-p    app_name@platform_alias     localdir    手动指定地址");
                 return 0;
         }
         else if (!strcmp("-d", argv[1]))
         {
                 if (argc < 3)
                 {
-                        puts("please input app_name");
+                        puts("please input : app_name");
                         return 0;
                 }
                 strcpy(app_name, argv[2]);
                 strcpy(local_name, "/opt/app_runtime");
                 strcpy(base_name, "/opt/base_runtime");
         }
-        else if (!strcmp("-p", argv[1]))
+        else if (!strcmp("-b", argv[1]))
         {
-                if (argc < 5)
+                if (argc < 3)
                 {
-                        puts("please input   base_path   local_path   app_name");
+                        puts("please input : app_name@platform_alias");
                         return 0;
                 }
-                strcpy(app_name, argv[4]);
+                split(argv[2], "@", appmsg_all);//分割app_name@platform_alias字符
+                
+                puts(appmsg_all[0]);puts(appmsg_all[1]);
+                
+                strcpy(app_name, appmsg_all[0]);
+                strcpy(local_name, "/opt/app_runtime");//默认local_runtime
+                strcpy(base_name, appmsg_all[1]);
+        }
+        else if (!strcmp("-c", argv[1]))
+        {
+                if (argc < 4)
+                {
+                        puts("please input : appName  localdir");
+                        return 0;
+                }
+                strcpy(app_name, argv[2]);
                 strcpy(local_name, argv[3]);
-                strcpy(base_name, argv[2]);
+                strcpy(base_name, "/opt/base_runtime");//默认base_runtime
+        }
+        else if (!strcmp("-p", argv[1]))
+        {
+                if (argc < 4)
+                {
+                        puts("please input ： app_name@platform_alias  localdir");
+                        return 0;
+                }
+                split(argv[2], "@", appmsg_all);//分割app_name@platform_alias字符
+
+                puts(appmsg_all[0]);puts(appmsg_all[1]);
+                
+                strcpy(app_name, appmsg_all[0]);
+                strcpy(local_name, argv[3]);
+                strcpy(base_name, appmsg_all[1]);
         }
         else
         {
                 puts("输入错误");
                 return 0;
         }
+
         sprintf(msg_all,"%s-%s-%s", app_name, local_name, base_name);
 
         puts(msg_all);
